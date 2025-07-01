27414add78964a33b84b005158816d9271c021e7
YURY PABLO DA SILVA PEREIRA <202511011830015@ifrn.local>
2025-07-01 10:56:22 -0300

    Tela de Pesquisa feita

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+import WelcomeScreen from './Telas/Welcomescreen';
+import SearchScreen from './Telas/SearchScreen';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <SafeAreaView style={{ flex: 1}}>
+
+      <WelcomeScreen />
+      <SearchScreen />
+
+      </SafeAreaView>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
